50e2ae46ee379ce78d6f2a065ffe8acc20c6853a
MarkyMan4 <markus.hutnik@gmail.com>
2021-10-24 22:24:13 -0500

    added some textures

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+textures = ['white_cube', 'grass', 'brick']
+selected_texture = 0
+
 app = Ursina()
 window.borderless = False
 window.exit_button.visible = False
 
 class Voxel(Button):
-    def __init__(self, position=(0,0,0)):
+    def __init__(self, position=(0,0,0), texture='white_cube'):
         super().__init__(
             parent=scene,
             position=position,
             model='cube',
             origin_y=0.5,
-            texture='white_cube',
+            texture=texture,
             color=color.white,
             highlight_color=color.lime
         )
@@ -20,7 +23,7 @@ class Voxel(Button):
     def input(self, key):
         if self.hovered:
             if key == 'left mouse down':
-                Voxel(position=self.position + mouse.normal)
+                Voxel(position=self.position + mouse.normal, texture=textures[selected_texture % len(textures)])
 
             if key == 'right mouse down':
                 destroy(self)
@@ -36,7 +39,13 @@ def update():
         mouse.locked = False
 
     if held_keys['left mouse']:
-        print('yea')
         mouse.locked = True
+
+# this is better for detecting a key pressed, not held down
+def input(key):
+    global selected_texture
+
+    if key == 'tab':
+        selected_texture += 1
 
 app.run()
